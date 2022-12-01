2fb76e17c2468bc92f7c0f875e96c261b3697e3c
Huy Nguyen K <huy.nguyen@supremetech.vn>
2022-12-01 14:06:12 +0700

    Test

--- src/routes/home.js
+++ src/routes/home.js
@@ -7,7 +7,12 @@ const Home = () => {
     <div className="page">
       <AWSIcon />
       <h1>Deploy a SPA to AWS S3 and Cloudfront</h1>
-      <p>Set up Github action workflow deploy to S3 and invalidate Cloudfront</p>
+      <p>
+        Set up Github action workflow deploy to S3 and invalidate Cloudfront
+      </p>
+      <p>
+        Set up Github action workflow deploy to S3 and invalidate Cloudfront
+      </p>
       <Link to="/s3">What is AWS S3?</Link>
     </div>
   );
